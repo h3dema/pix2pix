4e24571bef49351553953d9a3711428251c910db
h3dema <henriquemoura@hotmail.com>
2025-02-05 11:24:06 +0100

    add dot

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,6 @@
-from discriminator import ConditionalDiscriminator
-from generator import UnetGenerator
-from criterion import GeneratorLoss, DiscriminatorLoss
+from .discriminator import ConditionalDiscriminator
+from .generator import UnetGenerator
+from .criterion import GeneratorLoss, DiscriminatorLoss
 
 
 __all__ = ['ConditionalDiscriminator', 'UnetGenerator', 'GeneratorLoss', 'DiscriminatorLoss']
